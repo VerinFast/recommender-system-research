2c4860ccbbcf7cfed7b7a1eb74ea12d223a7a812
Cole G <116569259+cgolding8@users.noreply.github.com>
2024-06-18 01:31:17 -0400

    RP-8 Multi-User Comparison Functions
    Can now find either the most similar user or a list of the most similar users compared to a given user across a population

--- research/recommender_system/matrix.py
+++ research/recommender_system/matrix.py
@@ -30,7 +30,7 @@ class RevMatrix:
 
 		Returns:
 				float: The sum of the number of positive reviews shared by both users and a random decimal value < 1
-    	(Randomness added so the first result with the same similarity score is not chosen every time)
+    	(Noise added so the first result with the same similarity score is not chosen every time)
 		"""
 		row1 = row1.copy(); row2 = row2.copy()
 		# Replace all non-positive reviews with 0 (including -1, np.nan, and 0)
@@ -50,7 +50,7 @@ class RevMatrix:
 
 		Returns:
 				float: The sum of the number of negative reviews shared by both users and a random decimal value < 1
-    	(Randomness added so the first result with the same similarity score is not chosen every time)
+    	(Noise added so the first result with the same similarity score is not chosen every time)
 		"""
 		row1 = row1.copy(); row2 = row2.copy()
 		# Replace all non-negative reviews with 0 (including 1, np.nan, and 0)
@@ -70,7 +70,7 @@ class RevMatrix:
 
 		Returns:
 				float: The sum of the number of positive and negative reviews shared by both users and a random decimal value < 1
-    	(Randomness added so the first result with the same similarity score is not chosen every time)
+    	(Noise added so the first result with the same similarity score is not chosen every time)
 		"""
 		row1 = row1.copy(); row2 = row2.copy()
 		# Combine both positive and negative counts
@@ -79,7 +79,7 @@ class RevMatrix:
 
 		return shared_likes + shared_dislikes + (random.random() if rand else 0)
 
-	def find_most_similar(self, user: ppl.Person, count_dislikes: bool = True,count_likes: bool = True,) -> ppl.Person:
+	def find_most_similar(self, user: ppl.Person, count_dislikes: bool = True, count_likes: bool = True) -> ppl.Person:
 		"""Finds the Person who's reviews are most similar to the provided user using the criteria provided.
 
 		Args:
@@ -92,7 +92,8 @@ class RevMatrix:
 				ppl.Person: The Person object containing the most similar reviews to the provided user
 		"""
 
-		# Find which comparison method to use, defaults to counting like and dislikes
+		# Find which comparison method to use, defaults to counting both likes and dislikes
+		# comp_list contains the total count of matching likes and/or dislikes shared between the two users
 		if not count_dislikes:
 			comp_list = np.apply_along_axis(self.count_shared_likes, 1, self.matrix, user.reviews)
 		elif not count_likes:
@@ -100,13 +101,49 @@ class RevMatrix:
 		else:
 			comp_list = np.apply_along_axis(self.count_shared_likes_and_dislikes, 1, self.matrix, user.reviews)
 
-		# Find the top two most similar results (in case first result is user.review, in which case the other result is returned)
+		# Find the indexes of the top two most similar results (in case first result is user.review, in which case the other index is returned)
 		idx_most_similar: list[int] = heapq.nlargest(2, range(len(comp_list)), key = comp_list.__getitem__)
 
-  	# Return the non-matching result
+  	# Return the non-matching Person object
 		recommend_not_same: int = idx_most_similar[-1] if self.pop.people[idx_most_similar[0]] == user else idx_most_similar[0]
 		
 		return self.pop.people[recommend_not_same]
+
+	def find_all_most_similar(self, user: ppl.Person, count_dislikes: bool = True, count_likes: bool = True) -> list[ppl.Person]:
+		"""Finds the Person(s) who's reviews are most similar to the provided user using the criteria provided.
+
+		Args:
+				user (ppl.Person): The base user to find the most similar Person to
+				count_dislikes (bool, optional): Whether to count likes (+1) ratings when finding similarities, defaults to True
+				count_likes (bool, optional): Whether to count dislikes (-1) ratings when finding similarities, defaults to True
+		* If both count_likes and count_dislikes are True / False, they will both be counted
+
+		Returns:
+				ppl.Person: All Person objects containing the most similar reviews to the provided user
+		"""
+
+		# Find which comparison method to use, defaults to counting both likes and dislikes
+		# "comp_list" contains the total count of matching likes and/or dislikes shared between the two users
+		if not count_dislikes:
+			comp_list = np.apply_along_axis(self.count_shared_likes, 1, self.matrix, user.reviews)
+		elif not count_likes:
+			comp_list = np.apply_along_axis(self.count_shared_dislikes, 1, self.matrix, user.reviews)
+		else:
+			comp_list = np.apply_along_axis(self.count_shared_likes_and_dislikes, 1, self.matrix, user.reviews)
+
+		# Find the indexes of the top two most similar results (in case first result is user.review, in which case the other index is returned)
+		idx_most_similar: list[int] = heapq.nlargest(2, range(len(comp_list)), key = comp_list.__getitem__)
+
+  	# Return the index of the non-matching Person object
+		recommend_not_same: int = idx_most_similar[-1] if self.pop.people[idx_most_similar[0]] == user else idx_most_similar[0]
+
+		# Get noiseless comp value from comp_list
+		best_comp: int = math.floor(comp_list[recommend_not_same])
+
+		# Find list of indexes with matching noiseless comp values
+		best_comps: npt.NDArray[np.int_] = np.flatnonzero(comp_list.astype(int) == best_comp)
+
+		return [self.pop.people[i] for i in best_comps]
 
 
 
