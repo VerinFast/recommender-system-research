8e1c7d58776eaba23c2943a5de97c60fb605780d
Cole G <116569259+cgolding8@users.noreply.github.com>
2024-06-27 00:49:27 -0400

    Update Experiment Variables
    Set up independent control variables to be more in line with the desired final experiment

--- research/recommender_system/controls.py
+++ research/recommender_system/controls.py
@@ -2,10 +2,10 @@
 """A list of constant variables that can be used to manipulate the experiment"""
 ###--------------------------------------------------------------------------###
 
-MATRIX_SIZE: int = 50
+MATRIX_SIZE: int = 20
 """The size (# of users / goods) used to create a (n x n) matrix for the experiment"""
 
-NUMBER_OF_TICKS: int = 100
+NUMBER_OF_TICKS: int = 10
 """The amount of time / number of loops that the experiment should run for"""
 
 UTILITY_MEAN: int = 4
@@ -20,10 +20,10 @@ USER_BUDGET: int = 10
 CONSIDERATION_COST: int = 1
 """The "cost" to the user when looking for a good to be recommended to them"""
 
-USAGE_COST: int = 1
+USAGE_COST: int = 5
 """The "cost" to the user when using the good that has been recommended to them"""
 
-COUNT_NEGATIVE_REVIEWS: bool = False
+COUNT_NEGATIVE_REVIEWS: bool = True
 """Whether or not to count negative reviews when finding recommendations for a user"""
 
 
@@ -31,7 +31,7 @@ COUNT_NEGATIVE_REVIEWS: bool = False
 """A list of constant variables used in (research/recommender_system/__init__.py)"""
 ###------------------------------------------------------------------------------###
 
-BLANK_REP: str = "#"
+BLANK_REP: str = "·"
 """The character used to represent an empty (NoneType object) space in matrix printouts"""
 
 SEED: int = 10
@@ -42,14 +42,14 @@ SEED: int = 10
 """A list of constant variables used for testing purposes"""
 ###------------------------------------------------------###
 
-NEG_PERCENT: float = 0.1
+NEG_PERCENT: float = 0.075
 """Percent (weighted chance) of randomly generated user reviews containing a negative review (-1)"""
 
-ZERO_PERCENT: float = 0.2
+ZERO_PERCENT: float = 0.35
 """Percent (weighted chance) of randomly generated user reviews containing a neutral / no review (0)"""
 
-POS_PERCENT: float = 0.1
+POS_PERCENT: float = 0.075
 """Percent (weighted chance) of randomly generated user reviews containing a positive review (1)"""
 
-BLANK_PERCENT: float = 0.6
+BLANK_PERCENT: float = 0.5
 """Percent (weighted chance) of randomly generated user reviews that have not been used (np.nan)"""
